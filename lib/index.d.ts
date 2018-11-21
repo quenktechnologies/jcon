4752c8203b9635d27d99915a467218872a15a6eb
Lasana Murray <lmurray@quenk.com>
2018-11-21 18:10:59 -0400

    chore: rebuild

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,11 @@
 /// <reference path="parser.d.ts" />
 import * as ast from './ast';
+import { Either } from '@quenk/noni/lib/data/either';
 import { Nodes, Node } from './ast';
+/**
+ * Result of attempting to parse a source text.
+ */
+export declare type Result<N extends Node> = Either<Error, N>;
 /**
  * tree is a map of reference nodes that can be used during parsing.
  */
@@ -8,4 +13,4 @@ export declare const tree: Nodes<Node>;
 /**
  * parse source text into an abstract syntax tree.
  */
-export declare const parse: <N extends ast.Node>(str: string, ast: ast.Nodes<N>) => N;
+export declare const parse: <N extends ast.Node>(str: string, ast: ast.Nodes<N>) => Either<Error, N>;
--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,13 @@
 /// <reference path='parser.d.ts' />
 import * as ast from './ast';
+import {Either, left, right} from '@quenk/noni/lib/data/either';
 import { Nodes, Node } from './ast';
 import parser = require('./parser');
+
+/**
+ * Result of attempting to parse a source text.
+ */
+export type Result<N extends Node> = Either<Error, N>;
 
 /**
  * tree is a map of reference nodes that can be used during parsing.
@@ -11,9 +17,18 @@ export const tree: Nodes<Node> = <any>ast;
 /**
  * parse source text into an abstract syntax tree.
  */
-export const parse = <N extends Node>(str: string, ast: Nodes<N>): N => {
+export const parse = <N extends Node>(str: string, ast: Nodes<N>): Result<N> => {
 
-    parser.parser.yy = { ast };
-    return parser.parser.parse(str);
+  parser.parser.yy = { ast };
+
+  try {
+    
+    return right(parser.parser.parse(str));
+
+  }catch(e) {
+
+    return left(e);
+
+  }
 
 }
